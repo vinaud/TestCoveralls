6f164e317d3be5ac74c996cc66c1e0fa650bfe83
Pablo Bedoya <pablo.reds@hotmail.com>
2013-12-02 14:00:18 -0300

    Consertando classes

--- src/test/java/Test_addMagazine_EC_PW.java
+++ src/test/java/Test_addMagazine_EC_PW.java
@@ -1,7 +1,11 @@
+package test.java;
+
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import main.java.Paperround;
 
 import org.junit.Before;
 import org.junit.Test;
